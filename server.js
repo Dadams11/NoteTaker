9bcca3c0b7e3045d3a74f36427898d1a69922c2d
Dadams11 <adams.1743@osu.edu>
2023-07-23 19:13:05 -0400

    updated API routes in server.js file

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// server.js
-
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,7 +11,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Route to handle the GET request for retrieving all saved notes
-app.get('/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
   const notes = dbUtils.readDataFromFile();
   res.json(notes);
 });
@@ -26,6 +24,11 @@ app.post('/api/notes', (req, res) => {
 // Route to handle the DELETE request for deleting a note by its ID
 app.delete('/api/notes/:id', (req, res) => {
   // ... (code to delete a note by ID from db.json and respond with the updated list of notes) ...
+});
+
+// Catch-all route to serve the 'notes.html' file for any other route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
 // Start the server
